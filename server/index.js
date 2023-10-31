c30ced356c3e2dcbecbd70802f5749d2d3d3bf70
Diogo Sobral <dsforasteiro@gmail.com>
2023-10-31 20:45:50 -0300

    First Commit

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const got = require("got");
+const fs = require("fs");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.listen(5000, "0.0.0.0", () => {
+    console.log("Servidor On!");
+});
+
+app.get("/animes", async(req, res) => {
+    const { name } = req.query;
+
+    const response = await got('https://api.jikan.moe/v4/anime?q=' + name, { json: true });
+    const dados = response.body.data;
+    let answer = '<div style="display: flex; gap: 20px; flex-wrap: wrap; justify-content: center; align-items: center">';
+    for (i = 0; i < dados.length; i++) {
+        answer += '<div style="width: 170px"><img src="' + dados[i].images.webp.large_image_url + '" alt="' + dados[i].title + '" style="width: 150px"></br>'
+        answer += dados[i].title + '</div>';
+    }
+    answer += '</div>';
+    res.send(answer);
+});
+
+app.get("/recomendations", async(req, res) => {
+    function getUniqueListBy(arr, key) {
+        return [...new Map(arr.map(item => [item[key], item])).values()]
+    }
+
+    const response = await got('https://api.jikan.moe/v4/recommendations/anime', { json: true });
+    const dados = response.body.data;
+    let answer = [];
+    for (i = 0; i < dados.length; i++) {
+        answer.push({ "mal_id": dados[i].entry[0].mal_id, "title": dados[i].entry[0].title, "image": dados[i].entry[0].images.webp.large_image_url });
+        answer.push({ "mal_id": dados[i].entry[1].mal_id, "title": dados[i].entry[1].title, "image": dados[i].entry[1].images.webp.large_image_url });
+    }
+    answer = getUniqueListBy(answer, 'mal_id');
+    res.send(answer);
+});
