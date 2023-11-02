2c27ee1fa92fc6bea7505ecf0a12ef140b92a50a
Thiago Fernandes <86969851+fivvvve@users.noreply.github.com>
2023-11-01 23:34:57 -0300

    Menu da página
    Menu principal da aplicação utilizando rotas para cada página feito. Componente especifico para a página de Animes feito e melhoria no layout das recomendações

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import "./NavBar.css";
+import { useLocation } from "react-router-dom";
+import NavItem from "./NavItem";
 
 function NavBar() {
+  let { pathname } = useLocation();
+  const NavBarData = [
+    {
+      title: "Animes",
+      path: "/",
+    },
+    {
+      title: "Top Animes",
+      path: "/topanimes",
+    },
+    {
+      title: "List It",
+      path: "/listit",
+    },
+    {
+      title: "To Watch",
+      path: "/towatch",
+    },
+    {
+      title: "Watched",
+      path: "/watched",
+    },
+  ];
   return (
-    <div>NavBar</div>
-  )
+    <nav className="menu">
+      <ul>
+        {NavBarData.map((item) => {
+          return (
+            <NavItem {...item} key={item.path} isSelected={item.path === pathname}/>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
