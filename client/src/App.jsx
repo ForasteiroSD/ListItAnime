782b6dc5dbd6da28c52a9f2f3745d38664f26f6e
Diogo Sobral <dsforasteiro@gmail.com>
2023-11-04 00:57:48 -0300

    Changing white and dark mode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+//Components
 import "./App.css";
 import NavBar from "./components/NavBar";
 import NotFound from "./components/NotFound";
@@ -11,6 +12,9 @@ import Watched from "./pages/Watched";
 
 //Packages
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {useState, useEffect} from 'react'
+import {MdDarkMode} from 'react-icons/md';
+import {BsFillBrightnessHighFill} from 'react-icons/bs';
 
 //Css
 import "./App.css";
@@ -19,16 +23,22 @@ import "./App.css";
 import { toggleMode } from "./utils/toggleMode";
 
 function App() {
+  const [themeIcon, setThemeIcon] = useState()
+
+  const changeIcon = () => {
+    const newMode = toggleMode();
+    if(newMode === 'dark') setThemeIcon(<BsFillBrightnessHighFill />);
+    else setThemeIcon(<MdDarkMode />);
+  }
+
+  useEffect(() => {
+    changeIcon();
+  }, [])
+
   return (
     <>
-      <button className="toggleMode" onClick={toggleMode}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="1em"
-          viewBox="0 0 512 512"
-        >
-          <path d="M512 256c0 .9 0 1.8 0 2.7c-.4 36.5-33.6 61.3-70.1 61.3H344c-26.5 0-48 21.5-48 48c0 3.4 .4 6.7 1 9.9c2.1 10.2 6.5 20 10.8 29.9c6.1 13.8 12.1 27.5 12.1 42c0 31.8-21.6 60.7-53.4 62c-3.5 .1-7 .2-10.6 .2C114.6 512 0 397.4 0 256S114.6 0 256 0S512 114.6 512 256zM128 288a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm0-96a32 32 0 1 0 0-64 32 32 0 1 0 0 64zM288 96a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm96 96a32 32 0 1 0 0-64 32 32 0 1 0 0 64z" />
-        </svg>
+      <button className="toggleMode" onClick={changeIcon}>
+        {themeIcon}
       </button>
       <Router>
         <NavBar />
--- client/src/utils/toggleMode.js
+++ client/src/utils/toggleMode.js
@@ -1,13 +1,13 @@
-
 export function toggleMode() {
     const root = document.querySelector(':root');
     if (root.style.getPropertyValue('--background') === '#121214') {
-        root.style.setProperty('--background', '#F8F9FB');
+        root.style.setProperty('--background', '#e9e9e9');
         root.style.setProperty('--white', 'white');
-        root.style.setProperty('--border-nav', '#EFEFF1');
+        root.style.setProperty('--border-nav', 'rgb(225, 225, 225)');
         root.style.setProperty('--text', '#272D37');
-        root.style.setProperty('--background-nav', 'white');
+        root.style.setProperty('--background-nav', 'rgb(241, 241, 241)');
         root.style.setProperty('--highlight', '#574EFA');
+        return 'white';
     } else {
         root.style.setProperty('--background', '#121214');
         root.style.setProperty('--white', 'black');
@@ -15,5 +15,6 @@ export function toggleMode() {
         root.style.setProperty('--text', 'white');
         root.style.setProperty('--background-nav', '#202024');
         root.style.setProperty('--highlight', '#4e84fa');
+        return 'dark';
     }
 }
