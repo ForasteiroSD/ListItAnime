c56c8df308d65babeae473013559f3a3ca058f0e
Thiago Fernandes <86969851+fivvvve@users.noreply.github.com>
2023-11-24 20:44:09 -0300

    Responsividade e conteúdos página About

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -10,41 +10,57 @@ function About() {
           <h1 className="animeName">ABOUT</h1>
         </div>
 
-        <div className='info'>
+        <div className="info">
           <p>
-            This is a web made application that let the users save their whatched animes and animes to watch, while create a list of highest rated animes by the user. The application was create with the intuite to help its creators to rate their watched animes and as the final project of the course <i>Programação Web</i>.
+            This is a web made application that let the users save their
+            whatched animes and animes to watch, while create a list of highest
+            rated animes by the user. The application was created with the
+            intuite to help its creators to rate their watched animes and as the
+            final project of the course <i>Programação Web</i>.
           </p>
         </div>
 
-        <h2>ABOUT US</h2>
+        <h2 className="animeName">ABOUT US</h2>
         <div className="aboutUs">
           <div className="data">
             <figure>
               <img
-                src="https://pbs.twimg.com/media/FB1gPxUXoAQh5oO?format=jpg&name=small"
+                src="https://avatars.githubusercontent.com/u/94070943?v=4"
                 alt=""
               />
             </figure>
             <div className="texts">
-              <p>name</p>
-              <p>link github</p>
-              <p>n sei oq mais</p>
+              <p>Diogo Sobral</p>
+              <a href="https://github.com/ForasteiroSD" target="__blank">
+                Link GitHub
+              </a>
             </div>
           </div>
           <div className="data">
+            <div className="texts">
+              <p>Thiago Fernandes</p>
+              <a href="https://github.com/fivvvve" target="__blank">
+                Link GitHub
+              </a>
+            </div>
             <figure>
               <img
-                src="https://pbs.twimg.com/media/FB1gPxUXoAQh5oO?format=jpg&name=small"
+                src="https://avatars.githubusercontent.com/u/86969851?v=4"
                 alt=""
               />
             </figure>
-            <div className="texts">
-              <p>name</p>
-              <p>link github</p>
-              <p>n sei oq mais</p>
-            </div>
           </div>
         </div>
+        <footer>
+          <p>
+            <i>
+              "Às vezes, as perguntas são complicadas e as respostas são
+              simples."
+            </i>
+            <br />
+            L. Lawliet (Death Note)
+          </p>
+        </footer>
       </div>
     );
 }
