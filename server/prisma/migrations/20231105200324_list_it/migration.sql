29d2d92aac7c47937efc2fd55c810932ef16354a
Thiago Fernandes <86969851+fivvvve@users.noreply.github.com>
2025-02-23 01:09:05 -0300

    Update database

--- server/prisma/migrations/20231105200324_list_it/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Anime" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "image" TEXT NOT NULL,
-    "listId" TEXT NOT NULL,
-    "position_score" REAL NOT NULL,
-    CONSTRAINT "Anime_listId_fkey" FOREIGN KEY ("listId") REFERENCES "List" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
